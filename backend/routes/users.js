b13ae875574e9a7d74db6b50c871a0f6d0a6ecba
AmanyAbd5 <amanydawa5@gmail.com>
2022-12-05 10:28:02 +0200

    amany commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,28 +1,45 @@
 var express = require('express');
 var router = express.Router();
-
+const db = require("../config/dbconfig");
+const orderBy=require("lodash/orderBy");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-/*
-router.post("/adduser",function (request, response){
-  console.log("data recive from client", request.body);
-  const options = {
-      // schema is not passed here since it has been passed while creating client
-      table: 'users',
-      records: [request.body],
-    };
-    db.insert(options, (err, res) => {
-      if(err) {
-          console.log("error in add user operation ",err);
-          response.status(500).send({"error":err});
-      }
-      else {
-          console.log(res);
-          response.send({"result":"User added successfulley"});
-      }
-    });
+
+
+/////////////////////////////////////////////////////////////////////////////////
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tweetapplication"
 });
-*/
+
+//con = require('./connection');
+
+
+router.post("/adduser",function(request, response){
+
+  var id =request.body.id;
+  var Username =request.body.Username;
+  var Fullname =request.body.Fullname;
+  var Birthday =request.body.Birthday;
+  var Password =request.body.Password;
+  var Address =request.body.Address;
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO users (id,Username,Fullname,Birthday,Password,Address) VALUES ('"+id+"', '"+Username+"','"+Fullname+"','"+Birthday+"','"+Password+"','"+Address+"')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("user added successfulley");
+      response.send({"result":"user added successfulley"});
+    });
+  });
+});
+//////////////////////////////////////////////////////////////////////////
 module.exports = router;
